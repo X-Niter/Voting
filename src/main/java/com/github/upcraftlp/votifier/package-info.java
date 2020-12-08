03674ca141a0a5da52149c7bab4d7b2836e328c5
Beardedflea <Linzell.Godfrey@live.co.uk>
2020-12-08 13:27:22 +0000

    Start of v2.0

--- src/main/java/com/github/upcraftlp/votifier/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * (c)2018 UpcraftLP
- */
-@MethodsReturnNonnullByDefault @ParametersAreNonnullByDefault
-package com.github.upcraftlp.votifier;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
